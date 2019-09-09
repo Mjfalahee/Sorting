64c51e81fba250831779fef7f6048eff65840430
mjfalahee <mjfalahee@gmail.com>
2019-09-09 14:50:39 -0700

    Both of the binary searches done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -24,10 +24,26 @@ def binary_search(arr, target):
   if len(arr) == 0:
     return -1 # array empty
     
-  low = 0
+  low = 0 
   high = len(arr)-1
 
   # TO-DO: add missing code
+
+  while low <= high:
+    middle = (low+high)//2
+    # print('Middle is now: ', middle)
+
+    if (arr[middle] == target):
+      # print('Target found: ', middle)
+      return middle
+
+    elif arr[middle] < target:
+        low = middle + 1
+        # print('Low is now: ', low)
+
+    else:
+        high = middle - 1
+        # print('High is now: ', high)
 
   return -1 # not found
 
@@ -40,3 +56,17 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+
+  if (high >= low): 
+    if (arr[middle] == target):
+      return middle
+    
+    elif arr[middle] < target:
+      return binary_search_recursive(arr, target, middle+1, high)
+
+    else:
+      return binary_search_recursive(arr, target, low, middle-1)
+  else:
+    return -1
+    
+
