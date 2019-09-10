50d0cfd38ac1a8c00b13fe715991a37ef39ff91d
mjfalahee <mjfalahee@gmail.com>
2019-09-10 14:11:57 -0700

    merging helper function done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,11 +3,33 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    for i in range(0, elements): 
+        if ((len(arrA) > 0) and (len(arrB) > 0)): #this is only done if there are values to compare
+
+            if arrA[0] < arrB[0]:
+                value = arrA.pop(0)
+                merged_arr[i] = value
+
+            elif arrB[0] < arrA[0]: 
+                value = arrB.pop(0)
+                merged_arr[i] = value
+
+        else: #if one list is empty, see which one it is and put the last values from the remaining list into the merged one.
+            if (len(arrA) == 0):
+                value = arrB.pop(0)
+                merged_arr[i] = value
+            else:
+                value = arrA.pop(0)
+                merged_arr[i] = value
+            
+    print('merged', merged_arr)
     return merged_arr
 
-
+merge([1,3,4,7], [2,5,6,8])
 # TO-DO: implement the Merge Sort function below USING RECURSION
+# base case is an array of size 1
+# divide everything up
+# sort those
 def merge_sort( arr ):
     # TO-DO
 
